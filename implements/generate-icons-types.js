78ea5da4e3024c1580449ee4768ddb3653bfdf75
Konstantin Che <konstantin24121@gmail.com>
2018-09-06 22:55:54 +0300

    Fix generate icons types

--- implements/generate-icons-types.js
+++ implements/generate-icons-types.js
@@ -8,12 +8,25 @@ const manifestFileName = path.join(
 );
 if (fs.existsSync(manifestFileName)) fs.unlinkSync(manifestFileName);
 
+function camelize(str) {
+  const regexp = new RegExp(/([a-zA-Z]{1}[a-z]*)/g);
+  let match;
+  match = regexp.exec(str);
+  let camilizeStr = match[0].toLowerCase();
+  while ((match = regexp.exec(str)) !== null) {
+    const matchStr = match[0];
+    camilizeStr += matchStr.substr(0, 1).toUpperCase() + matchStr.substring(1);
+  }
+  return camilizeStr;
+}
+
 fs.readdir(path.join(global.boil.src, 'static/icons'), function(err, items) {
   let content = 'const icons = {';
   let declaration = 'export enum EIconNames {';
   for (var i = 0; i < items.length; i++) {
-    declaration += `${path.basename(items[i], '.svg')},`;
-    content += `[EIconNames.${path.basename(items[i], '.svg')}]: require('icons/${items[i]}'),`;
+    const name = camelize(path.basename(items[i], '.svg'));
+    declaration += `${name},`;
+    content += `[EIconNames.${name}]: require('icons/${items[i]}'),`;
   }
   declaration += '}';
   content += '};export {icons};';
