6330d2b55db29ed3c3e4d0915462a4976e37f6b3
VinstonVolf <konstantin24121@gmail.com>
2019-01-19 01:14:28 +0200

    Make fetch user epic simpler

--- src/common/reduck/modules/user/epics.ts
+++ src/common/reduck/modules/user/epics.ts
@@ -1,7 +1,7 @@
 import { ofType } from '@martin_hotell/rex-tils';
 import * as action from './actions';
 import { ActionsObservable, StateObservable } from 'redux-observable';
-import { delay, mapTo, mergeMap, map } from 'rxjs/operators';
+import { delay, mapTo, mergeMap, map, switchMap } from 'rxjs/operators';
 import { Api } from './api';
 import { UserInfo } from './models';
 import { from } from 'rxjs';
@@ -19,14 +19,15 @@ export const increment = (
 export const fetchUser = (action$: ActionsObservable<action.TActions>) =>
   action$.pipe(
     ofType(action.ETypes.FetchUserInfo),
-    mergeMap((catchedAction) =>
-      from(Api.getUserInfo(catchedAction.payload.login)).pipe(
-        map((response) => {
-          const userInfo = UserInfo.createFromJson(response);
-          return action.Actions.setUserInfo(userInfo);
-        }),
-      ),
-    ),
+    switchMap(async (catchedAction) => {
+      try {
+        const response = await Api.getUserInfo(catchedAction.payload.login);
+        const userInfo = UserInfo.createFromJson(response);
+        return action.Actions.setUserInfo(userInfo);
+      } catch (e) {
+        console.log(e)
+      }
+    }),
   );
 
 export default [increment, fetchUser];
